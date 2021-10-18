d708541b69fcb61c6cc3490987bc1479f5f747d4
Tunahan Berber <tunahanberber123@gmail.com>
2021-10-19 00:04:08 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+console.log(dolarDun)
+
+let euroDun = 11
+
+console.log(euroDun)
+
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi", "Özel Konut Kredisi"]
+console.log("<ul>")
+for (let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>" +konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
